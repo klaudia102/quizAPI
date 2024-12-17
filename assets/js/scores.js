a85639a9680aef396529f791be57fe4b93929dca
klaudia102 <146132740+klaudia102@users.noreply.github.com>
2024-12-17 19:21:25 +0000

    addedf code for retriving data from local storage

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,7 +1,46 @@
+let clearBtn = document.querySelector('#clear');
+let scoreList = document.querySelector('#highscores');
 // retrive hight score from local storage 
-const highScores = JSON.parse(localStorage.getItem('highScores'))
+
+var highScoresFromLocalStorage = JSON.parse(localStorage.getItem('highscores'))
+
+console.log(highScoresFromLocalStorage)
+
+init()
 
 // sort the scorts from hightes to lowest
 // display
+function renderScoresList() {
+    for (var i = 0; i < highScoresFromLocalStorage.length; i++) {
+        var entry = highScoresFromLocalStorage[i];
+    
+        var li = document.createElement("li");
 
-//remove from local storage
+        li.textContent = entry;
+        li.setAttribute("data-index", highScoresFromLocalStorage[i].score);
+    
+        scoreList.appendChild(li);
+      }
+      console.log('nks')
+}
+
+
+function init() {
+    
+    
+  
+    if (highScoresFromLocalStorage !== null) {
+      highScores = highScoresFromLocalStorage;
+    }
+console.log
+    renderScoresList();
+  }
+
+
+// //remove from local storage
+
+// function clearStorage() {
+//     clearStorage()
+// }
+
+// clearBtn.addEventListener('click', clearStorage)
